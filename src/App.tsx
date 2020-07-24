27cd1523f50863e5c987a14de2942381ebd9ae53
Julia Fong <Juliamzfong@gmail.com>
2020-07-23 22:35:35 -0700

    Add styling files

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import QuestionCard from './components/QuestionCard';
 //Types
 import { QuestionState, Difficulty } from './API';
 // Styles
-import { GlobalStyle, Wrapper } from './App.styles';
+import { GlobalStyle } from './App.styles';
 
 export type AnswerObject = {
   question: string
@@ -47,7 +47,7 @@ const App = () => {
       // Check answer against correct answer
       const correct = questions[number].correct_answer === answer;
       // Add score if answer is correct
-      if (correct) setScore(prev => prev +1);
+      if (correct) setScore(prev => prev + 1);
       // Save answer in the array for user answers
       const answerObject = {
         question: questions[number].question,
@@ -71,35 +71,37 @@ const App = () => {
 };
 
   return (
-    
-    <div className="App">
-      <h1> React Quiz</h1>
-      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
-        <button className="start" onClick={startTrivia}>
-          Start Trivia
-        </button> 
-      ): null}
-      {!gameOver? <p className="score">Score:</p> : null }
-      {loading && <p>Loading Questions ...</p>}
-      {!loading && !gameOver && (
-        <QuestionCard 
-          questionNum={number + 1}
-          totalQs={TOTAL_QUESTIONS}
-          question={questions[number].question}
-          answers={questions[number].answers}
-          userAnswer={userAnswers ? userAnswers[number]: undefined}
-          callback={checkAnswer}
-        />
-      )} 
-      {!gameOver && 
-      !loading && 
-      userAnswers.length === number + 1 
-      && number !== TOTAL_QUESTIONS - 1 ? (
-        <button className="next" onClick={nextQuestion}>
-          Next Question
-        </button>
-      ): null} 
-    </div>   
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <h1> React Quiz</h1>
+        {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+          <button className="start" onClick={startTrivia}>
+            Start Trivia
+          </button> 
+        ): null}
+        {!gameOver? <p className="score">Score:</p> : null }
+        {loading && <p>Loading Questions ...</p>}
+        {!loading && !gameOver && (
+          <QuestionCard 
+            questionNum={number + 1}
+            totalQs={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number]: undefined}
+            callback={checkAnswer}
+          />
+        )} 
+        {!gameOver && 
+        !loading && 
+        userAnswers.length === number + 1 
+        && number !== TOTAL_QUESTIONS - 1 ? (
+          <button className="next" onClick={nextQuestion}>
+            Next Question
+          </button>
+        ): null} 
+      </div>   
+    </>
     ) 
     
 }
---
+++ src/components/QuestionCard.styles.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import QuestionCard from './QuestionCard';
+
+
+export default QuestionCard
