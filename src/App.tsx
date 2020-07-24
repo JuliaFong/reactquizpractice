6ab7fab6f5c3a6105bece0419bdbea672583af05
Julia Fong <Juliamzfong@gmail.com>
2020-07-23 21:28:24 -0700

    Update hooks

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import { fetchQuizQuestions } from './API';
 import QuestionCard from './components/QuestionCard';
 //Types
 import { QuestionState, Difficulty } from './API';
+// Styles
+import { GlobalStyle, Wrapper } from './App.styles';
 
-type AnswerObj = {
+export type AnswerObject = {
   question: string
   answer: string
   correct: boolean
@@ -18,7 +20,7 @@ const App = () => {
   const [loading, setLoading] = useState(false)
   const [questions, setQuestions] = useState<QuestionState[]>([])
   const [number, setNumber] = useState(0)
-  const [userAnswers, setUserAnswers] = useState([])
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([])
   const [score, setScore] = useState(0)
   const [gameOver, setGameOver] = useState(true)
 
